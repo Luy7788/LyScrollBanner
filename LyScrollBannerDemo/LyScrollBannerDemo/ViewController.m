fac729b745726c740355a2e4432f82b4a1396ac7
Lying <lying_555@163.com>
2016-07-23 01:58:19 +0800

    啦啦啦

--- LyScrollBannerDemo/LyScrollBannerDemo/ViewController.m
+++ LyScrollBannerDemo/LyScrollBannerDemo/ViewController.m
@@ -37,7 +37,9 @@
     CGRect  rect1 = CGRectMake(0, 300, 320, 220);
     NSArray *arrHttp = @[@"http://pic9.nipic.com/20100909/3398346_111317208068_2.jpg",@"http://pic8.nipic.com/20100719/668573_214559007865_2.jpg",@"http://pic104.nipic.com/file/20160720/19451547_082719257000_2.jpg",@"http://pic104.nipic.com/file/20160720/19451547_082531991000_2.jpg"];
 
-    LyScrollBanner *ScrollBanner1 = [LyScrollBanner LyScrollBannerWithFrame:rect1 ImagesArrayWtithNetworking:arrHttp placeholderImageName:@"loading.jpg"];
+    LyScrollBanner *ScrollBanner1 = [LyScrollBanner LyScrollBannerWithFrame:rect1
+                                                 ImagesArrayWtithNetworking:arrHttp
+                                                       placeholderImageName:@"loading.jpg"];
     ScrollBanner1.delegate = self;
     [self.view addSubview:ScrollBanner1];
 }
